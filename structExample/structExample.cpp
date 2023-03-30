654d080347635faf3b03d74b0803e68fbedb3bd3
Michwynn <michael.wynn23@hotmail.com>
2023-03-30 19:09:30 +0800

    Updated structure example.

---
+++ structExample/structExample.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+struct staff {
+    // declare members of the structure staff
+    char name[40]{};
+    char role[10]{};
+    int age{};
+};
+
+int main() {
+    // declare variables of type staff
+    struct staff staff_1 = { "Jane Doe", "Admin", 20 };
+    // print the details of the staff staff_1;
+    printf("Details of staff 1 :\n");
+    printf("\tName : %s\n", staff_1.name);
+    printf("\tAge : %d\n", staff_1.age);
+    printf("\tRole : %s\n", staff_1.role);
+
+    return 0;
+}
